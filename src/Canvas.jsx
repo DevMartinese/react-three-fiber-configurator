6e0b894b13793a5910e95939f7a5fff9b5096f2e
DevMartinese <devmartinese@gmail.com>
2024-03-25 23:56:35 -0300

    add: new eading zoom

--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -96,6 +96,7 @@ function CameraRig({ children }) {
   const group = useRef();
 
   useFrame((state, delta) => {
+    easing.damp3(state.camera.position, [0, 0, 2], 0.25, delta)
     easing.dampE(
       group.current.rotation,
       [state.pointer.y / 10, -state.pointer.x / 5, 0],
